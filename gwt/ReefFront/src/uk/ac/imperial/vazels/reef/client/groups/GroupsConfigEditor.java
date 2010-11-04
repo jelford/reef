feec8fbf460b8901ee6ca3ee0ba0650effd9f523
James Elford <james.elford08@imperial.ac.uk>
2010-11-04 12:03:25 +0000

    Updated gitignore

---
+++ gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/groups/GroupsConfigEditor.java
@@ -0,0 +1,10 @@
+package uk.ac.imperial.vazels.reef.client.groups;
+
+import com.google.gwt.user.client.ui.Composite;
+
+public class GroupsConfigEditor extends Composite {
+
+  public GroupsConfigEditor() {
+  }
+
+}
