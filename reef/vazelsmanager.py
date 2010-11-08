5b36455491e42d519ca36f119f387bfba5f86147
James Elford <james.elford08@imperial.ac.uk>
2010-11-08 11:39:59 +0000

    Now correctly parses groups and specifies them when the server is run.

--- reef/vazelsmanager.py
+++ reef/vazelsmanager.py
@@ -8,19 +8,20 @@ def getVazelsPath():
 def runVazels():
   experiment_path = os.path.join(config.getSettings("global")['projdir'], "experiment")
   
-  # TODO: Build the groups string
-  groups_string=""
-    
-    
+  groups_string = "--groups="
+  group_data = config.getSettings("groups")
+  groups_string += ':'.join([str(group)+','+str(group_data[group]['size']) for group in group_data])
+  
   os.chdir(getVazelsPath())
   command_line = "vazels_control_centre.sh"
   command_line += " --root_dir="+experiment_path+"/"
-  command_line += " --groups="+groups_string
+  command_line += " "+groups_string
   command_line += " --ssh_server_identity=" + config.getSettings("global")['certificate']
   command_line += " " + config.getSettings("command_centre")["rmi"]
   command_line += " " + config.getSettings("command_centre")["siena"]
   
   # TODO: Change this so that it pipes its output to some handler!
+  print(command_line)
   os_return_value = os.system("sh " + command_line + " &")
    
   if os_return_value == 0:
