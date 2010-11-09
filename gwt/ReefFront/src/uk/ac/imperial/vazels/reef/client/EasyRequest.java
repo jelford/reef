3dd2f9fc3a00d55d496c2f8be7ca82c153e86295
Andy <as.gurden@gmail.com>
2010-11-09 23:12:06 +0000

    Introduced JavaDocs for the MultipleRequester class to make it usable by all members of the group without further explanation
    Made quick edit from a few other branches to MultipleRequester's go() method
    
    This keeps behaviour from other classes that use the old go(),
    but adds an optional string to specify an address extension
    
    JavaDocs in AddressResolution
    
    Fixed AddressResolution docs.
    
    And again, still learning :)
    
    JavaDoc'ed EasyRequest
    
    JavaDoc'ed RequestHandler
    
    JavaDoc'ed MultipleRequester
    
    Also added a few finals where methods shouldn't be overriden in
    MultipleRequester and EasyRequest
    
    Because git was complaining, I'm uploading these silly classes.

--- gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/EasyRequest.java
+++ gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/EasyRequest.java
@@ -7,6 +7,14 @@ import com.google.gwt.http.client.RequestException;
 import com.google.gwt.http.client.Response;
 import com.google.gwt.http.client.URL;
 
+/**
+ * Easy way to make requests.
+ * <p>
+ * This automatically uses {@link AddressResolution} to determine the correct
+ * address to poke. To use you must subclass this with your own request handler.
+ * Note that is is much easier to use {@link MultipleRequester} to make requests.
+ * </p>
+ */
 public abstract class EasyRequest
 {
 	private AddressResolution addRes;
@@ -16,11 +24,30 @@ public abstract class EasyRequest
 		addRes = new AddressResolution();
 	}
 	
+	/**
+	 * 
+	 * Initialise assuming we are in testing (or not).
+	 * 
+	 * @param inTesting Are we in testing?
+	 * 
+	 * @deprecated Use {@link EasyRequest#EasyRequest()}.
+	 * 
+	 * This remains only for legacy code that may have used it when address
+	 * resolution was here.
+	 */
 	public EasyRequest(boolean inTesting)
 	{
 		addRes = new AddressResolution(inTesting);
 	}
 	
+	/**
+	 * Send a request.
+	 * 
+	 * @param method The {@link RequestBuilder.Method} to use (POST or GET).
+	 * @param url The URL to request (e.g. "/settings/").
+	 * @param query A list of {@link QueryArg}s to pass with the request.
+	 * @return A {@link RequestTicket} to identify the response.
+	 */
 	public RequestTicket request(RequestBuilder.Method method, String url, QueryArg[] query)
 	{
 	  final RequestTicket ticket = new RequestTicket();
@@ -71,8 +98,29 @@ public abstract class EasyRequest
 	
 	// Sets all if it can.
 	// If there was a problem with the request, everything is null.
+	/**
+	 * Callback for any responses.
+	 * <p>
+	 * This will always receive a ticket identifying the originating request.
+	 * If something goes wrong with sending or receiving the response, all
+	 * other parameters will be {@code null}. Otherwise the parameters will
+	 * be filled in where possible (depending on the returned code).
+	 * </p>
+	 * 
+	 * @param ticket Identifies the originating request.
+	 * @param code The returned response code.
+	 * @param reason The text following the response code.
+	 * @param content A {@link String} containing the response data (without headers).
+	 */
 	protected abstract void requested(RequestTicket ticket, Integer code, String reason, String content);
 	
+	/**
+	 * Used to when sending a request to hold the arguments.
+	 * <p>
+	 * These are in the usual name=value pairs that you would see in the query
+	 * string of a GET request.
+	 * </p>
+	 */
 	public static class QueryArg
 	{
 		private final String name;
@@ -94,6 +142,13 @@ public abstract class EasyRequest
 	    return value;
     }
 		
+		/**
+		 * Takes {@link QueryArg#getName} and {@link QueryArg#getValue()} to
+		 * provide a string of the form used in application/x-www-form-urlencoded data
+		 * <p>
+		 * i.e. name=value
+		 * </p>
+		 */
 		public String toString(){
 		  if(getValue() == null)
 		    return URL.encode(getName());
@@ -102,7 +157,9 @@ public abstract class EasyRequest
 		}
 	}
 	
-	// Given on each request to identify the reply
+	/**
+	 * Used to identify requests. This does not define any new methods.
+	 */
 	public class RequestTicket{
 	  public boolean equals(RequestTicket t){
 	    return this == t;
--- gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/MultipleRequester.java
+++ gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/MultipleRequester.java
@@ -5,12 +5,47 @@ import java.util.Map;
 
 import com.google.gwt.http.client.RequestBuilder;
 
+/**
+ * Currently the preferred way to make any application/x-www-form-urlencoded requests.
+ * <p>
+ * It allows the user to send multiple requests, each with it's own individual
+ * handler that is matched back to it when a response is returned.
+ * </p>
+ * <p>
+ * The preferred way to use this is to subclass it for each type of request you would
+ * like to make. So one per address usually. In the new constructor you should pass in
+ * all the defaults the particular request will need. This allows you to use your
+ * subclass with a default constructor, avoiding errors.
+ * </p>
+ * <p>
+ * There are a couple of methods you can override to affect behaviour:
+ * <ul>
+ * <li>{@link MultipleRequester#received(Object, boolean, String)}</li>
+ * <li>{@link MultipleRequester#getArgs()}</li>
+ * </ul>
+ * </p>
+ * <p>
+ * And use {@link MultipleRequester#go} to actually send a request.
+ * </p>
+ *
+ * @param <Type> The type that should be returned by a response to one
+ * of these requests.
+ */
 public class MultipleRequester<Type> extends EasyRequest {
   private Map<RequestTicket, RequestHandler<Type>> handlers;
   private final String addr;
   private final RequestBuilder.Method method;
   private final Converter<Type> converter;
   
+  /**
+   * Set up the factory request details.
+   * 
+   * @param method {@link RequestBuilder.Method} to use (POST or GET)
+   * @param addr The address to use (e.g. "/settings/")
+   * @param converter To convert between the response body, a {@link String} to
+   * the correct {@code <Type>}
+   * @throws NullPointerException If any of the parameters are {@code null}
+   */
   public MultipleRequester(RequestBuilder.Method method, String addr, Converter<Type> converter){
     if(addr == null || method == null || converter == null)
       throw new NullPointerException();
@@ -20,8 +55,16 @@ public class MultipleRequester<Type> extends EasyRequest {
     this.converter = converter;
   }
   
-  @Override
-  protected void requested(RequestTicket ticket, Integer code, String reason, String content) {
+  /**
+   * @deprecated Use {@link MultipleRequester#received(Object, boolean, String)}
+   * <p>
+   * This should only be called from the base class {@link EasyRequest} as the
+   * callback method. It will actually deal with calling the correct handlers here
+   * so must not be overriden. This will avoid breaking the expectations of other
+   * callbacks.
+   * </p>
+   */
+  protected final void requested(RequestTicket ticket, Integer code, String reason, String content) {
     RequestHandler<Type> handler = handlers.get(ticket);
     
     if(isSuccess(code)){
@@ -35,17 +78,65 @@ public class MultipleRequester<Type> extends EasyRequest {
     handlers.remove(ticket);
   }
   
-  // Override to supply arguments
+  /**
+   * Provides the query arguments for any request.
+   * <p>
+   * This is called for each request and should be overridden if the request
+   * is to provide any {@link QueryArg}s. In current implementations of this
+   * method, we subclass {@link MultipleRequester} as an inner class of the
+   * class using it. This means that we can rip query arguments straight from
+   * the class that contains the relevant data.
+   * </p>
+   * 
+   * @return A list of {@link QueryArg}s to provide with the request.
+   */
   protected QueryArg[] getArgs(){
     return null;
   }
   
-  // Override to process before the actual handler is called
+  /**
+   * A callback that is run for <strong>every</strong> received response.
+   * <p>
+   * This has the same input as
+   * {@link RequestHandler#handle(Object, boolean, String)} and is called directly
+   * before the individual response handler.
+   * </p>
+   * <p>
+   * You should only override this if you need to use it.
+   * </p>
+   */
   protected void received(Type reply, boolean success, String message){
   }
   
+  /**
+   * Send a request.
+   * <p>
+   * Use all the request information given in the constructor to send a request.
+   * If {@code handler} is not {@code null} we use it to handle any received
+   * response.
+   * </p>
+   * 
+   * @param handler The handler that is called on result of this request.
+   */
   public void go(final RequestHandler<Type> handler){
-    RequestTicket ticket = request(method, addr, getArgs());
+    go(handler, "");
+  }
+  
+  /**
+   * Send a request to a modified address.
+   * <p>
+   * This works just as {@link MultipleRequester#go(RequestHandler)} but modifies
+   * the address that the request is sent to.
+   * </p>
+   * 
+   * @param handler The handler that is called on result of this request.
+   * @param addrExt A {@link String} tagged to the end of the request address.
+   * For instance a request constructed with the request address "/settings/"
+   * and with {@code addrExt} as "global" will send the request to
+   * "/settings/global".
+   */
+  public void go(final RequestHandler<Type> handler, String addrExt){
+    RequestTicket ticket = request(method, addr+addrExt, getArgs());
     handlers.put(ticket, new RequestHandler<Type>() {
       @Override
       public void handle(Type reply, boolean success, String message) {
@@ -74,7 +165,18 @@ public class MultipleRequester<Type> extends EasyRequest {
     return (code != null && code == 200);
   }
   
+  /**
+   * Implement this to show we can convert to {@code <Type>}.
+   * 
+   * @param <Type> The type to convert to.
+   */
   protected interface Converter<Type>{
+    /**
+     * Converts a {@link String} to the given type.
+     * 
+     * @param original Input string.
+     * @return Output object of type {@code <Type>}.
+     */
     public Type convert(String original);
   }
 }
