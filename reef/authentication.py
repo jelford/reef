27cb34615728aa044aac4f0e02e79f33c188df74
Andy <as.gurden@gmail.com>
2010-11-24 21:05:58 +0000

    Authentication doesn't ask for login with no users

--- reef/authentication.py
+++ reef/authentication.py
@@ -4,6 +4,7 @@ import config
 config.getSettings("auth").setdefault("users", {})
 
 # the model cannot be stored in the config, but cache it here
+# This should always match the users dict (None is empty)
 authModel = None
 
 # Add user to the config and update the model
@@ -12,9 +13,10 @@ def addUser(name, password):
     users = config.getSettings("auth")["users"]
 
     if name in users:
-        raise KeyError, str(name)+"already exists"
+        raise KeyError, str(name)+" already exists"
 
     users[name] = password
+    # Don't need a complete refresh to add one user
     if authModel is None:
         refresh()
     else:
@@ -23,14 +25,17 @@ def addUser(name, password):
 # Refresh the model
 def refresh():
     global authModel
-    authModel = restlite.AuthModel()
 
     users = config.getSettings("auth")["users"]
-    for user in users:
-        addUserToModel(user, users[user])
+    if not users.keys():
+        authModel = None
+    else:
+        authModel = restlite.AuthModel()
+        for user in users:
+            addUserToModel(user, users[user])
 
 
-# Add user to the current model
+# Add user to the current model (assuming it exists)
 def addUserToModel(user, password):
     global authModel
     authModel.register(
@@ -40,14 +45,19 @@ def addUserToModel(user, password):
     )
 
 
-# Get (and possibly create) the current model
-def getModel():
-    global authModel
-    if authModel is None:
-        refresh()
-    return authModel
-
-
 # Login with the given request
 def login(request):
-    request.login(getModel())
+    global authModel
+    # If there are not users, don't ask for login
+    if authModel is not None:
+        request.login(model)
+
+
+# Clear all authentication (removes users and turns auth off)
+def clear():
+    config.getSettings("auth")["users"] = {}
+    refresh()
+
+
+# Set up here
+refresh()
