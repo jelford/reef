ba1c25be2cb6bc8ab02cbb3e816c49244d749217
Andy <as.gurden@gmail.com>
2010-11-26 13:11:43 +0000

    Modified the easystart script to be friendlier to multiple projays

---
+++ deploy/clean.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+CURDIR=`pwd`
+cd `dirname $0`
+
+rm -rf pro_*
+
+cd $CURDIR
--- deploy/easystart.sh
+++ deploy/easystart.sh
@@ -1,9 +1,33 @@
 #!/bin/sh
 
+CURDIR=`pwd`
+
 cd `dirname $0`
 
-mkdir project
+get_proname()
+{
+  FNAME=$1
+  BASE=`basename $FNAME`
+  echo ${BASE#pro_}
+}
 
-cd project
+#bob
 
-python ../reef/startup.py
+PRONAME="pro_default"
+
+if [ $# -eq 0 ]; then
+  echo "Current projects are:"
+  echo ""
+  for file in $( find . -name pro_* -print )
+  do
+    get_proname $file
+  done
+else
+  PRONAME="pro_$1"
+
+  mkdir $PRONAME
+  cd $PRONAME
+  python ../reef/startup.py
+fi
+
+cd $CURDIR
