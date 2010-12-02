982962aa48be456d449ad3995087477a96b5926b
James Elford <james.elford08@imperial.ac.uk>
2010-12-02 18:10:59 +0000

    Added non-functional client side code to start talking about the SUE

---
+++ gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/sue/SueComponentAllocator.java
@@ -0,0 +1,10 @@
+package uk.ac.imperial.vazels.reef.client.sue;
+
+import com.google.gwt.user.client.ui.Composite;
+
+public class SueComponentAllocator extends Composite {
+
+  public SueComponentAllocator() {
+  }
+
+}
