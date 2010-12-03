953e4e9edd7b06345d947f01f502a1561ce3ddec
Andy <as.gurden@gmail.com>
2010-12-03 17:07:57 +0000

    Make GroupSummary match the new server.

--- gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/groups/GroupSummary.java
+++ gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/groups/GroupSummary.java
@@ -1,7 +1,7 @@
 package uk.ac.imperial.vazels.reef.client.groups;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 import com.google.gwt.core.client.JsArrayString;
@@ -13,72 +13,51 @@ import com.google.gwt.core.client.JsArrayString;
  * @author james
  *
  */
-public class GroupSummary { 
+public class GroupSummary implements Iterable<String>{ 
   /**
    * We'll store a map of the groups
    */
-  private Map<String,Integer> groups;
+  private Set<String> groups;
   
   /**
    * If no argument is given, will assume an empty list of groups.
    */
   public GroupSummary() {
-    /* Initialize with an empty JSON string */
-    initialize("{}");
+    this(null);
   }
   
   /**
    * Given a JSON input, will construct the map
-   * for you.
+   * for you. {@code null} will create an empty summary.
    * @param jsonInput
    */
   public GroupSummary(String jsonInput) {
-    initialize(jsonInput);
-  }
-  
-  private void initialize(String jsonInput) {
-    GroupSummaryOverlay thisObject = parseJSON(jsonInput);
-    groups = new HashMap<String,Integer>();
-    
-    final JsArrayString keys = thisObject.keys();
-    final int keysLength = keys.length();
-    for (int i=0; i<keysLength; i++) {
-      String key = keys.get(i);
-      groups.put(key, new Integer(thisObject.lookup(key)));
+    groups = new HashSet<String>();
+    if(jsonInput != null) {
+      JsArrayString names = parseArray(jsonInput);
+      for(int i=0; i<names.length(); i++)
+        groups.add(names.get(i));
     }
   }
   
-  private native final GroupSummaryOverlay parseJSON(String json) /*-{
-    return JSON.parse(json);
+  protected native JsArrayString parseArray(String json) /*-{
+    return eval(json);
   }-*/;
   
-  public int get(final String key) {
-    return groups.get(key);
-  }
-  
-  public Set<String> keySet() {
-    return groups.keySet();
-  }
-  
-  public Integer put(String newGroupName, Integer newGroupSize) {
-    return groups.put(newGroupName, newGroupSize);
-  }
-  
-  public Integer remove(String groupToRemove) {
-    return groups.remove(groupToRemove);
+  /**
+   * Get the number of groups.
+   * @return number of groups.
+   */
+  public int length() {
+    return groups.size();
   }
 
-  public boolean contains(String newGroupName) {
-    return groups.containsKey(newGroupName);
-  }
-  
-  public int size() {
-    int numberOfEmptyGroups=0;
-    for (String key : groups.keySet()) {
-      if (groups.get(key) == 0) {
-        numberOfEmptyGroups++;
-      }
-    }
-    return groups.size() - numberOfEmptyGroups;
+  /**
+   * Get an iterator, DO NOT REMOVE!
+   * @return an iterator...
+   */
+  @Override
+  public Iterator<String> iterator() {
+    return groups.iterator();
   }
 }
