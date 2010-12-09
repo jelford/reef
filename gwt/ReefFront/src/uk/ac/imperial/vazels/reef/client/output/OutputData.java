2a0d805b3c6b0e97ee705d7bf4138054aafa3026
Simon <simon@simon-laptop.(none)>
2010-12-09 00:30:42 +0000

    Started to create the layout for the output data to be displayed. Fixed problem with gwt not being able to use JSON libraries.

--- gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/output/OutputData.java
+++ gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/output/OutputData.java
@@ -1,5 +1,13 @@
 package uk.ac.imperial.vazels.reef.client.output;
 
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONValue;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.Widget;
+
 
 public class OutputData {
   
@@ -18,7 +26,6 @@ public class OutputData {
   
   private void initialise(String jsonInput) {
     data = jsonInput;
-
   }
   
   
@@ -27,12 +34,55 @@ public class OutputData {
   }
   
   public void setData(String jsonInput) {
-    
-    
-      
-    
+    data = jsonInput;
   }
   
+  public Panel getPanel() {
+    
+    FlowPanel outputPanel = new FlowPanel();
+    System.out.println(getData());
+    JSONArray groups = JSONParser.parse(getData()).isArray();
+    
+    /*if(groups == null) {
+        return outputPanel;
+    }*/
+    System.out.println("foo");
+    for(int i = 0; i < groups.size(); i++) {
+        outputPanel.add(new Label("Group " + i));
+        outputPanel.add(groupHTMLHelper(groups.get(i)));
+    }
+    
+    return outputPanel;
+  }
   
+  Panel groupHTMLHelper(JSONValue group) {
+    FlowPanel groupPanel = new FlowPanel();
+    
+    JSONArray hosts = group.isArray();
+    if(hosts == null) {
+      return groupPanel;
+    }
+    for(int i = 0; i < hosts.size(); i++) {
+      
+      groupPanel.add(new Label("Host " + i));
+      groupPanel.add(hostHTMLHelper(hosts.get(i)));
+      
+    }
+    
+    return groupPanel;
+  }
+  
+  Panel hostHTMLHelper(JSONValue host) {
+    FlowPanel hostPanel = new FlowPanel();
+    
+    JSONArray data = host.isArray();
+    if(data == null) {
+      return hostPanel;
+    }
+    for(int i = 0; i < data.size(); i++) {
+      
+    }
+    return hostPanel;
+  }
   
 }
