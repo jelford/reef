eef32413673f2a6386f7450cae495ae71881e50f
Andy <as.gurden@gmail.com>
2010-12-12 19:45:00 +0000

    Added a generic converter for use in multiple managers

---
+++ gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/managers/JsArrayStringSetConverter.java
@@ -0,0 +1,27 @@
+package uk.ac.imperial.vazels.reef.client.managers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.google.gwt.core.client.JsArrayString;
+
+import uk.ac.imperial.vazels.reef.client.MultipleRequester.Converter;
+
+/**
+ * Converts a Javascript array of strings to a set of strings.
+ */
+public class JsArrayStringSetConverter implements Converter<Set<String>>{
+  @Override
+  public Set<String> convert(String original) {
+    JsArrayString array = parseJson(original);
+    Set<String> set = new HashSet<String>();
+    for(int i=0; i<array.length(); i++) {
+      set.add(array.get(i));
+    }
+    return set;
+  }
+  
+  public final native JsArrayString parseJson(String json) /*-{
+    return eval(json);
+  }-*/;
+}
