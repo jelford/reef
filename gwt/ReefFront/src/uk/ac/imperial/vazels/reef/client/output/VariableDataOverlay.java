b6ea9d90c705be4dff8ba00b07ada3eabf088655
Simon <simon@simon-laptop.(none)>
2010-12-13 01:06:19 +0000

    Finalised construction of tree. All that remains is to display selected data in the required side panel

--- gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/output/VariableDataOverlay.java
+++ gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/output/VariableDataOverlay.java
@@ -1,9 +1,26 @@
 package uk.ac.imperial.vazels.reef.client.output;
 
 import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArrayString;
 
 public class VariableDataOverlay extends JavaScriptObject {
   
   protected VariableDataOverlay() { }
+
+  native final JsArrayString keys() /*-{
+    
+    var keys = [];
+    
+    for (key in this) {
+      keys.push(key);
+    }
+    return keys;
+  }-*/;
+  
+  native final TimeSeriesOverlay get(String key) /*-{
+  
+    return this[key];
+  
+  }-*/;
 
 }
