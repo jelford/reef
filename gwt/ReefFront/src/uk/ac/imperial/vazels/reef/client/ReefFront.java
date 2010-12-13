50ec37434a50fc63deac920097a4d96e3cc1c47d
Andreea Babiuc <eb808@ic.ac.uk>
2010-12-13 18:41:38 +0000

    gui with css

--- gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/ReefFront.java
+++ gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/ReefFront.java
@@ -2,7 +2,7 @@ package uk.ac.imperial.vazels.reef.client;
 
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Image;
+
 import com.google.gwt.user.client.ui.RootPanel;
 
 import uk.ac.imperial.vazels.reef.client.ReefTabPanel;
@@ -16,20 +16,20 @@ public class ReefFront implements EntryPoint
 	 * This is the entry point method.
 	 */
 	
+  static String initialHTML;
+  
   public void onModuleLoad()
 	{
+    initialHTML = RootPanel.get().getBodyElement().getInnerHTML();
+     
 	  //RootPanel.get("settingstest").add(new SettingsWgt());
 	 // RootPanel.get("specify-groups").add(new AllocateGroups());
 	 // RootPanel.get("server-control").add(new ServerControl());
 	 // RootPanel.get("workloads").add(new WorkloadWidget());
 	 // RootPanel.get("actors").add(new UploadActorWidget());
 
+     
     
-//    
-//    final Image image = new Image();
-//    image.setUrl("reeffront/gwt/neon/images/logo.png");
-//    RootPanel.get().add(image);
-//     
     RootPanel.get("tabPanel").add(new ReefTabPanel());
 
 
