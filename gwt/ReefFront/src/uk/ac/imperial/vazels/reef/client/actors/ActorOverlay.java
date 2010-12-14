5f3601a9d5065fa165794b6ed0a0dbd4308722ee
Chris <chris@chris-VirtualBox.(none)>
2010-12-14 18:18:01 +0000

    Actor manager

---
+++ gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/actors/ActorOverlay.java
@@ -0,0 +1,15 @@
+package uk.ac.imperial.vazels.reef.client.actors;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+public class ActorOverlay extends JavaScriptObject {
+  protected ActorOverlay() {}
+
+  public final native String getName() /*-{
+      return this.name;
+    }-*/;
+
+  public final native String getLanguage() /*-{
+      return this.language;
+    }-*/;
+}
