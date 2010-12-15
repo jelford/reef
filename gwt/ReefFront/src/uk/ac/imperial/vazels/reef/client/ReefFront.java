48a74b623144c37e3bd6936d84894baa42f88ab6
Andy <as.gurden@gmail.com>
2010-12-15 21:31:07 +0000

    Cleaned up entry point.
    This invovled
    
    * Removing unecessary storage of the previous page body
    * Removing commented out versions of all the widgets we used to have here
    * Making the JavaDocs at least a little helpful

--- gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/ReefFront.java
+++ gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/ReefFront.java
@@ -8,30 +8,15 @@ import com.google.gwt.user.client.ui.RootPanel;
 import uk.ac.imperial.vazels.reef.client.ReefTabPanel;
 
 /**
- * Entry point classes define <code>onModuleLoad()</code>.
+ * Main entry point for the reef application.
  */
 public class ReefFront implements EntryPoint
 {
 	/**
-	 * This is the entry point method.
+	 * Starting point.
 	 */
-	
-  static String initialHTML;
-  
   public void onModuleLoad()
 	{
-    initialHTML = RootPanel.get().getBodyElement().getInnerHTML();
-     
-	  //RootPanel.get("settingstest").add(new SettingsWgt());
-	 // RootPanel.get("specify-groups").add(new AllocateGroups());
-	 // RootPanel.get("server-control").add(new ServerControl());
-	 // RootPanel.get("workloads").add(new WorkloadWidget());
-	 // RootPanel.get("actors").add(new UploadActorWidget());
-
-     
-    
     RootPanel.get("tabPanel").add(new ReefTabPanel());
-
-
 	}
 }
