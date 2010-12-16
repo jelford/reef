cf31e520901e05920e20bd192ccf825229da29f9
Andy <as.gurden@gmail.com>
2010-12-16 20:11:31 +0000

    Moved css out into its own file and cleaned the styling up a little. Added interface to css.

---
+++ gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/ui/UsefulCss.java
@@ -0,0 +1,13 @@
+package uk.ac.imperial.vazels.reef.client.ui;
+
+import com.google.gwt.resources.client.CssResource;
+
+public interface UsefulCss extends CssResource {
+  public String spaced();
+  public String hStretch();
+  public String mostlyHStretch();
+  public String infoText();
+  public String noWrap();
+  public String shortInput();
+  public String majorComponentWidth();
+}
