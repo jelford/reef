693382734ec359f6c52f73ce357e59b5627216b6
Andy <as.gurden@gmail.com>
2010-12-21 03:49:55 +0000

    Prettied output from the javadoc generator

--- utilities/genJavaDocs.sh
+++ utilities/genJavaDocs.sh
@@ -8,33 +8,33 @@ rundir=`dirname $0`
 cd $rundir
 
 # Grab current branch name
-echo "Getting current branch..."
+echo -n "Getting current branch..."
 branchname="$(git symbolic-ref HEAD 2>/dev/null)" ||
 branchname="(unnamed branch)"     # detached HEAD
 branchname=${branchname##refs/heads/}
-echo "You are on branch $branchname"
+echo "$branchname"
 
 # Get the current commit
-echo "Getting latest commit..."
+echo -n "Getting latest commit..."
 commitsha="$(git rev-parse master)"
-echo "Your commit is $commitsha"
+echo "$commitsha"
 
 # Clear page-repo directory
-echo "Clearing repo space..."
-rm -rf $repo
-mkdir $repo
+echo -n "Clearing repo space..."
+rm -rf $repo &> /dev/null
+mkdir $repo &> /dev/null
 echo "All gone."
 
 # Move our repo in
-echo "Copying our current repo into itself..."
-cp -r ../.git $repo
+echo -n "Copying our current repo into itself..."
+cp -r ../.git $repo &> /dev/null
 echo "Done."
 
 # Move to our new repo and checkout gh-pages
-cd $repo
+cd $repo &> /dev/null
 
 # Get up to date pages repo
-echo "Grabbing most up to date version of the current documentation..."
+echo -n "Grabbing most up to date version of the current documentation..."
 # Go to master so we can remove the current doc repo
 git checkout master &> /dev/null
 # Then remove the pages branch
@@ -46,18 +46,21 @@ git pull origin gh-pages &> /dev/null
 echo "Got it, sorry if that took a while."
 
 # Get rid of the current javadoc dir and replace the contents
-echo "Clearing the javadoc directory..."
+echo -n "Clearing the javadoc directory..."
 git rm -r javadoc &> /dev/null
-rm -rf javadoc
-echo "..and filling it with the new docs..."
+rm -rf javadoc &> /dev/null
+echo "and filling it with the new docs..."
 javadoc -quiet -d javadoc -windowtitle "Reef JavaDocs" -private -sourcepath "../../gwt/ReefFront/src" -classpath "../jdoclibs/*:../../gwt/*" -subpackages uk.ac.imperial.vazels.reef 2> ../javadoc.out
 echo "Done."
 
 # Commit and push changes
-git add javadoc
-git commit -m "Added JavaDocs for the branch \"$branchname\" at commit: $commitsha" > /dev/null
+echo -n "Committing new pages..."
+git add javadoc &> /dev/null
+git commit -m "Added JavaDocs for the branch \"$branchname\" at commit: $commitsha" &> /dev/null
+echo "and pushing them"
 git push origin gh-pages
+echo "Finished!"
 
 # Remove remnants
-cd ..
-rm -rf $repo
+cd .. &> /dev/null
+rm -rf $repo &> /dev/null
