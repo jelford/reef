ec9aa4b89d795a9d8f2b1c769085ff1edf1f0fd4
Andy <as.gurden@gmail.com>
2010-12-22 17:56:16 +0000

    Added comitter for the python docs

---
+++ utilities/getDocs_python.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This will be run inside the git repo
+
+# Get rid of the current javadoc dir and replace the contents
+echo -n "Clearing the pydoc directory..."
+git rm -r pydoc &> /dev/null
+rm -rf pydoc &> /dev/null
+echo "and filling it with the new docs..."
+cp -r ../pydocs/build/html pydoc &> /dev/null
+echo "Done."
+
+# Commit changes
+echo -n "Committing new pages..."
+git add pydoc &> /dev/null
+git commit -m "Added PyDocs for the branch \"$branchname\" at commit: $commitsha" &> /dev/null
