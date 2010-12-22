6b0d4e64ce96e8760d2e918c777ffd1e16b94725
Andy <as.gurden@gmail.com>
2010-12-22 15:25:41 +0000

    Improved the documentation skeleton generator

--- utilities/pydocs/ensureAllDoced.sh
+++ utilities/pydocs/ensureAllDoced.sh
@@ -6,16 +6,22 @@ function writeExcluded {
   echo "./startup.py"
 }
 
-pydocs=$(cd `dirname $0`; pwd)
-echo $pydocs
-docsrc="$pydocs/source"
+# Take a file path and echo a module name
+function getModule {
+  mod=${1%\.py}
+  mod=${mod#\./}
+  mod=$(echo "$mod" | sed 's/\//\./g')
+  
+  echo "$mod"
+}
 
-echo "Creating corresponding files for all the python source."
-echo "This should ensure we at least get a warning if we forget to document"
+# Take a module and give the file path for the rst file
+function getRst {
+  echo "$docsrc/$(echo "$1" | sed 's/\./\//g').rst"
+}
 
-cd `dirname $0`
-cd ../../reef
-
+# Take a pattern and echo the arguments to find needed for finding those files
+# If the pattern has a directory, this searches the whole name
 function getFindArgs {
   echo "."
   echo "("
@@ -29,31 +35,92 @@ function getFindArgs {
   echo "-prune"
   echo "-o"
   echo "("
-  echo "-name"
-  echo "*.py"
+  # If we have a directory in the pattern
+  if [[ "$1" =~ .*/.* ]]; then
+    echo "-wholename"
+  else
+    echo "-name"
+  fi
+  echo "$1"
   echo ")"
   echo "-print"
 }
 
-for file in $(getFindArgs | xargs find); do
-  base=${file%\.py}
-  base=${base#\./}
+# Find all python files that are subdirectories of the current directory
+# __init__.pys are left until the end and then the directory name is given
+function findPy {
+  inits=( )
+  for file in $(getFindArgs "*.py" | xargs find); do
+    if [ "`basename $file`" == "__init__.py" ]; then
+      # This is a directory module
+      inits=( "${inits[@]}" "`dirname $file`" )
+    else
+      echo $file
+    fi
+  done
+  for dir in "$inits"; do
+    echo $dir
+  done
+}
 
-  rstfile="$docsrc/$base.rst"
+# Pass a module and this will create the basic rst file for it
+function createRst {
+  module=$1
+  rstfile=$(getRst $module)
 
-  if [ ! -e "$rstfile" ]; then
-    echo "No .rst file for: $file"
-    echo "Creating one now..."
-    
-    mkdir -p `dirname $rstfile`
+  echo "  Creating doc file for $module..."
 
-    module=$(echo "$base" | sed 's/\//./g')
-    title="\`\`$module\`\` Module"
-    
-    underline=$(echo "$title" | sed 's/./=/g')
-    echo $title > $rstfile
-    echo $underline >> $rstfile
-    echo >> $rstfile
-    echo "  .. automodule:: $module" >> $rstfile
+  title="\`\`$module\`\` Module"
+  underline=$(echo "$title" | sed 's/./=/g')
+
+  mkdir -p `dirname $rstfile`
+  echo $title > $rstfile
+  echo $underline >> $rstfile
+  echo >> $rstfile
+  echo ".. automodule:: $module" >> $rstfile
+}
+
+pydocs=$(cd `dirname $0`; pwd)
+echo $pydocs
+docsrc="$pydocs/source"
+
+echo "Creating corresponding files for all the python source."
+echo "This should ensure we at least get a warning if we forget to document"
+
+cd `dirname $0`
+cd ../../reef
+
+# Go through each found module
+for file in $(findPy); do
+  module=$(getModule $file)
+  rst=$(getRst $module)
+
+  if [ ! -e "$rst" ]; then
+    echo "No doc file exists for the module: $module"
+    createRst $module
+
+    if [[ "$file" =~ .*\.py ]]; then
+      echo "  This module is just a basic module."
+    else
+      echo "  This module is a directory, adding ToC..."
+
+      echo >> $rst
+      echo ".. toctree::" >> $rst
+      echo "   :maxdepth: 1" >> $rst
+      echo >> $rst
+
+      thismodule=`basename $file`
+
+      for sub in $(cd $file; ls *.py); do
+        if [ "$sub" != "__init__.py" ]; then
+          mod=${sub%\.py}
+          echo "  Adding $sub"
+          echo "   $thismodule/$mod.rst" >> $rst
+        fi
+      done
+    fi
   fi
+
 done
+
+echo "Documentation skeleton generation complete."
