240d5dde5c9b9ef0eb3c0e778d4adec79d5126dc
Andy <as.gurden@gmail.com>
2010-12-23 13:49:21 +0000

    Added docstrings to permanent_bits

--- reef/permanent_bits.py
+++ reef/permanent_bits.py
@@ -1,5 +1,20 @@
+"""
+:synopsis: Holds constants to be used throughout the application.
+"""
+
+
 appname = "Reef"
+"""The short name of the application."""
+
 appname_file = "reef"
+"""The representation of the application to be used in file names."""
+
 appname_long = "Reef (Really Easy and Effortless Frontend)"
+"""The long version of the application title."""
+
 app_desc = "I would write a description here but that's a job for later..."
+"""The description for the application, used as a small blurb."""
+
 app_user = "reef"
+"""A user name to be used by default by the application."""
+
