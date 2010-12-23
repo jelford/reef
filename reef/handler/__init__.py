9fc55e921a1853b7cba0aa8cfb9934b1321c9dcf
Andy <as.gurden@gmail.com>
2010-12-23 13:10:12 +0000

    Added docstring for the handler module

---
+++ reef/handler/__init__.py
@@ -0,0 +1,9 @@
+"""
+:synopsis: Holds all the handlers to be used by the reef server.
+
+This is where any handlers should be placed.
+Ideally code not related to handling server requests should be elsewhere.
+
+To see where (or if) these handlers are attached to the server, see the
+:mod:`handlers` module.
+"""
