bb75f87235306ff2f857e8e57f0bbbb37b8eb8b5
Andy <as.gurden@gmail.com>
2010-12-23 22:58:07 +0000

    Added documentation for actor assignment

--- reef/handlers.py
+++ reef/handlers.py
@@ -14,7 +14,7 @@ from handler.testing import test_handler
 from handler.auth_page import auth_page_handler
 from handler.workloads import workload_handler
 from handler.actors import actors_handler
-from handler.actorassign import actorassign_handler
+from handler.actorassign import ActorAssignHandler
 from handler.control_handler import control_handler, start_handler, stop_handler, getallstatus_handler, getalloutput_handler, startexperiment_handler
 from handler.groups import group_handler, group_batch_handler
 from handler.status import status_handler
@@ -44,7 +44,7 @@ def getRouting():
         (r'GET,POST /actors$', 'GET,POST /actors/'),
         (r'GET,POST /actors/', actors_handler),
         (r'POST /actorassign$', 'POST /actorassign/'),
-        (r'POST /actorassign/', actorassign_handler),
+        (r'POST /actorassign/', ActorAssignHandler().getHandler()),
         (r'GET,POST /testing/', test_handler),
         (r'GET,POST /groups/?$', group_batch_handler),
         (r'GET,POST /groups/', group_handler),
