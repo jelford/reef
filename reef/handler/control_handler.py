f390e5ece577cefd9c19298c54c74d2a29707cc6
Andy <as.gurden@gmail.com>
2010-12-24 21:07:12 +0000

    Documented the control handlers

--- reef/handler/control_handler.py
+++ reef/handler/control_handler.py
@@ -1,90 +1,161 @@
+"""
+:synopsis: Creates an interface to the vazels system.
+
+"""
+
 import authentication
 import restlite
 import vazelsmanager
 
-### Manage starting & stopping the server ###
+from handler.dochandler import DocHandler
 
-@restlite.resource
-def control_handler():
-  def GET(request):
-    authentication.login(request)
-    runningState = vazelsmanager.vazelsRunning()
-    if runningState is True:
-      return request.response({"control_centre_status": "running"})
-    elif runningState is False:
-      return request.response({"control_centre_status": "ready"})
-    else:
-      return request.response({"control_centre_status": runningState})
-  
-  return locals()
+class ControlHandler(DocHandler):
+    """Handles status requests for the vazels control centre."""
 
-@restlite.resource
-def start_handler():
+    def GET(self, request):
+        """
+        Get the status of the vazels control centre.
+
+        :returns: The current control centre status, either
+
+                  * ``"{'control_centre_status': 'running'}"``
+                  * ``"{'control_centre_status': 'ready'}"``
+                  * ``"{'control_centre_status': <somethingelse>}"``
+
+                  where ``<somethingelse>`` is a return value of
+                  :func:`vazelsmanager.vazelsRunning()`
+
+        """
+
+        authentication.login(request)
+        runningState = vazelsmanager.vazelsRunning()
+        if runningState is True:
+            return request.response({"control_centre_status": "running"})
+        elif runningState is False:
+            return request.response({"control_centre_status": "ready"})
+        else:
+            return request.response({"control_centre_status": runningState})
+
+
+class StartHandler(DocHandler):
+    """Handles requests to start the vazels control centre."""
 ### Shouldn't need this at all ###
 #  def GET(request):
 #    authentication.login(request)
 #    raise restlite.Status("400 Invalid GET request to " + request["PATH_INFO"])
   
-  def POST(request, entity):
-    authentication.login(request)
-      
-    os_call_to_vazels = vazelsmanager.runVazels()
-      
-    if os_call_to_vazels :
-      # Need to upadate client to handle 204 as a successful response
-      return request.response("")
-    else :
-      raise restlite.Status("500 Vazels Control Centre failed to start")
-    
-  return locals()
+    def POST(self, request, entity):
+        """
+        Start the control centre.
 
-@restlite.resource
-def stop_handler():
-  
-  def POST(request,entity):
-    authentication.login(request)
-    
-    os_call_to_vazels = vazelsmanager.stopVazels()
-    
-    if os_call_to_vazels is True :
-      return request.response("")
-    else :
-      raise restlite.Status("500 "+str(os_call_to_vazels))
-      
-  return locals()
-  
-@restlite.resource
-def getallstatus_handler():
-  def GET(request):
-    authentication.login(request)
-    
-    if vazelsmanager.updateStatuses() is True:
-      return request.response("")
-    else :
-      raise restlite.Status, "400 Failed to send getallstatus request - don't call this before starting the Control Center"
-      
-  return locals()
+        :raises: :exc:`restlite.Status` 500 if something went immediately
+                 wrong while starting the control centre.
 
-@restlite.resource
-def getalloutput_handler():
-  def GET(request):
-    authentication.login(request)
-    
-    if vazelsmanager.getalloutput() is True:
-      return request.response("")
-    else :
-      raise restlite.Status, "400 Failed to get output from Vazels - don't call this before starting the Control Center"
-      
-  return locals()
+        .. todo:: Update client to handle 204 as a successful response.
+                  Then we dont have to send 200 with a blank response.
+
+        """
+
+        authentication.login(request)
+
+        os_call_to_vazels = vazelsmanager.runVazels()
+
+        if os_call_to_vazels:
+            # Need to upadate client to handle 204 as a successful response
+            return request.response("")
+        else:
+            raise restlite.Status("500 Vazels Control Centre failed to start")
+
+
+class StopHandler(DocHandler):
+    """Handles requests to stop the vazels control centre."""
+
+    def POST(self, request, entity):
+        """
+        Stop the control centre.
+
+        :raises: :exc:`restlite.Status` 500 if something went immediately
+                 wrong while stopping the control centre.
+
+        .. todo:: Update client to handle 204 as a successful response.
+                  Then we dont have to send 200 with a blank response.
+
+        """
+
+        authentication.login(request)
+
+        os_call_to_vazels = vazelsmanager.stopVazels()
+
+        if os_call_to_vazels is True:
+            return request.response("")
+        else:
+            raise restlite.Status("500 "+str(os_call_to_vazels))
+
   
-@restlite.resource
-def startexperiment_handler():
-  def GET(request):
-    authentication.login(request)
-    
-    if vazelsmanager.startexperiment() is True:
-      return request.response("")
-    else:
-      raise restlite.Status, "400 Failed to start experiment - don't call this before starting the Control Center"
-    
-  return locals()
+class GetAllStatusHandler(DocHandler):
+    """Handles status requests for hosts."""
+
+    def GET(self, request):
+        """
+        Update the groups with host statuses.
+
+        .. todo:: Make this POST!
+        .. todo:: Allow 204 as success in the client.
+
+        :raises: :exc:`restlite.Status` 400 if we cannot initiate an attempt
+                 to update.
+
+        """
+
+        authentication.login(request)
+
+        if vazelsmanager.updateStatuses() is True:
+            return request.response("")
+        else:
+            raise restlite.Status, "400 Failed to send getallstatus request - don't call this before starting the Control Center"
+
+
+class GetAllOutputHandler(DocHandler):
+    """Handles requests for vazels output."""
+
+    def GET(self, request):
+        """
+        Grab the latest output from the vazels experiment.
+
+        .. todo:: Make this POST!
+        .. todo:: Allow 204 as success in the client.
+
+        :raises: :exc:`restlite.Status` 400 if we could not start to
+                 retrieve data.
+
+        """
+
+        authentication.login(request)
+
+        if vazelsmanager.getalloutput() is True:
+            return request.response("")
+        else:
+            raise restlite.Status, "400 Failed to get output from Vazels - don't call this before starting the Control Center"
+
+
+class StartExperimentHandler(DocHandler):
+    """Handles requests to start a vazels experiment."""
+
+    def GET(self, request):
+        """
+        Try to start a vazels experiment.
+
+        .. todo:: Make this POST!
+        .. todo:: Allow 204 as success in the client.
+
+        :raises: :exc:`restlite.Status` 400 if the attempt to start an
+                 experiment failed immediately.
+
+        """
+
+        authentication.login(request)
+
+        if vazelsmanager.startexperiment() is True:
+            return request.response("")
+        else:
+            raise restlite.Status, "400 Failed to start experiment - don't call this before starting the Control Center"
