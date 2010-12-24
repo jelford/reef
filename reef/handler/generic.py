4ad02f3fed25959c554d91f7248316621f64093c
Andy <as.gurden@gmail.com>
2010-12-24 21:58:22 +0000

    Documented the generic handler.

--- reef/handler/generic.py
+++ reef/handler/generic.py
@@ -1,10 +1,28 @@
+"""
+:synopsis: Replaces the :mod:`restlite` usage message.
+
+"""
+
 import restlite
 
-### Serves 404 not found ###
+from handler.dochandler import DocHandler
 
-@restlite.resource
-def generic_page_handler():
-    def GET(request):
+class GenericPageHandler(DocHandler):
+    """
+    Used to replace :mod:`restlite`'s usage message when a request is not handled.
+
+    Instead, if inserted to handle pages at "/", this will return 404 for
+    **any** GET request.
+
+    """
+
+    def GET(self, request):
+        """
+        Return a 404 error.
+
+        :raise: :exc:`restlite.Status` 404...always!
+
+        """
+
         #return request.response(('path', request['PATH_INFO'], request['SCRIPT_NAME']))
         raise restlite.Status, "404 Not Found"
-    return locals()
