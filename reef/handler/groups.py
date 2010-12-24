11406b89d19e5f988379aa520cece7be9b153a5a
Andy <as.gurden@gmail.com>
2010-12-24 23:24:33 +0000

    Added documentation to groups handler.
    In addition spotted that the error raised for not finding a physical rank was
    500 - it's not a server error, so I changed it to 404.

--- reef/handler/groups.py
+++ reef/handler/groups.py
@@ -1,27 +1,39 @@
+"""
+:synopsis: Deals with any group related requests.
+
+"""
+
 import config
 import restlite
 import urlparse
 import authentication
 
-# Defined a set class that will serialize nicely so that it Python won't cry
-#   when we try to return it to the client (as json)
-class Group(dict):
-  def __init__(self, name):
-    self["name"] = name # We name all groups
-    self["size"] = 0 # Need to store the size
-    self["workloads"] = set([]) # Will store a list of workload names
-    self["filters"] = set([]) # Store a list of mapping restrictions (currently non-functional)
-    self["online_hosts"] = set([]) # Store a list of host names for connected & evolving hosts
-    self["evolving_hosts"] = set([])  
+from handler.dochandler import DocHandler
 
-  def _json_ (self) :
-    return {"name" : self["name"],
-            "size" : self["size"],
-            "workloads" : list(self["workloads"]),
-            "filters" : list(self["filters"]),
-            "online_hosts" : list(self["online_hosts"]),
-            "evolving_hosts" : list(self["evolving_hosts"])
-            }
+class Group(dict):
+    """
+    A class version of the old ``dict`` representation of a group.
+
+    This way Python won't cry when we try to serialise it as JSON.
+
+    """
+
+    def __init__(self, name):
+        self["name"] = name # We name all groups
+        self["size"] = 0 # Need to store the size
+        self["workloads"] = set([]) # Will store a list of workload names
+        self["filters"] = set([]) # Store a list of mapping restrictions (currently non-functional)
+        self["online_hosts"] = set([]) # Store a list of host names for connected & evolving hosts
+        self["evolving_hosts"] = set([])  
+
+    def _json_(self):
+        return {"name" : self["name"],
+                "size" : self["size"],
+                "workloads" : list(self["workloads"]),
+                "filters" : list(self["filters"]),
+                "online_hosts" : list(self["online_hosts"]),
+                "evolving_hosts" : list(self["evolving_hosts"])
+                }
 
 ### Allows for submitting groups ###
 
@@ -33,106 +45,174 @@ those of the form /groups/somethingMore are used for individual groups'''
 # def __newGroup() :
 #   return Group()
 
-@restlite.resource
-def group_batch_handler():
+class GroupBatchHandler(DocHandler):
+    """Handles requests for general group information."""
 
-  ## GET requests to this uri will return a summary of current groups
-  def GET(request):
-    authentication.login(request)
-    group_data = config.getSettings("groups").keys()
-    
-    return request.response(group_data)
-    
-  return locals()
+    def GET(self, request):
+        """
+        Get the list of groups on the server.
 
-@restlite.resource
-def group_handler():
-  
-  ## GET requests are for grabbing current info on a group
-  def GET(request):
-    authentication.login(request)
-   
-    # To find info on a group call /groups/groupName
-    group_name = request['PATH_INFO']
+        :returns: A list of groups.
+        :rtype: ``list``
 
-    try:
-      group_settings = config.getSettings("groups",True)[group_name]
-      return request.response(group_settings)
-    # Return 404 if they use an invalid group name
-    except KeyError:
-      raise restlite.Status, "404 Group Not Found"
-    
-  # POST requests will set up a group
-  def POST(request,entity):
-    authentication.login(request)
+        """
 
-    existing_groups = config.getSettings("groups")
-    group_name = request['PATH_INFO']
+        authentication.login(request)
+        group_data = config.getSettings("groups").keys()
 
-    args = urlparse.parse_qs(entity)
+        return request.response(group_data)
 
-    g_data = _getGroupDataFromArgs(args)
 
-    if group_name in existing_groups:
-      if "size" in g_data and g_data["size"] == 0:
-        # Delete the group
-        del existing_groups[group_name]
-      else:
-        existing_groups[group_name].update(g_data)
-    else:
+class GroupHandler(DocHandler):
+    """Handles requests for individual groups."""
 
-      # New group
-      n_group = Group(group_name)
-      n_group.update(g_data)
-      if n_group["size"] != 0:
-        existing_groups[group_name] = n_group
-    
-    try:
-      return request.response(existing_groups[group_name])
-    except KeyError:
-      return request.response({"name":group_name, "size":0})
+    ## GET requests are for grabbing current info on a group
+    def GET(self, request):
+        """
+        Get the JSON representation of a specific group.
 
-    # Cannot do as we are not sure the group exists
-    #return GET(request)
+        The group name is taken to be anything in the requested path after
+        the base address of the handler.
 
-  return locals()
+        :returns: JSON representation of the requested group.
+        :rtype: :class:`Group`
+        :raises: :exc:`restlite.Status` 404 if the group requested was not
+                 found.
+
+        """
+
+        authentication.login(request)
+
+        group_name = request['PATH_INFO']
+
+        try:
+            group_settings = config.getSettings("groups",True)[group_name]
+            return request.response(group_settings)
+        except KeyError:
+            raise restlite.Status, "404 Group Not Found"
+
+    # POST requests will set up a group
+    def POST(self, request, entity):
+        """
+        Create a new or edit an existing group.
+
+        The group name is taken to be anything in the requested path after
+        the base address of the handler.        
+
+        Parameters are expected to be part of the POST entity.
+
+        :param size: Group size.
+        :type size: ``int``
+        :param workloads: Can be included any number of times, each should be
+                          the name of an attached workload.
+        :type workloads: ``str``
+        :param filters: Can be included any number of times, each should be a
+                        mapping restriction.
+        :type filters: ``str``
+        :returns: JSON representation of the new state of the group.
+        :rtype: ``dict``
+        :raises: :exc:`restlite.Status` 400 if the size is invalid.
+
+                 :exc:`restlite.Status` 400 unrecognised parameters are
+                 included.
+
+        """
+
+        authentication.login(request)
+
+        existing_groups = config.getSettings("groups")
+        group_name = request['PATH_INFO']
+
+        args = urlparse.parse_qs(entity)
+        g_data = _getGroupDataFromArgs(args)
+
+        if group_name in existing_groups:
+            if "size" in g_data and g_data["size"] == 0:
+                # Delete the group
+                del existing_groups[group_name]
+            else:
+                existing_groups[group_name].update(g_data)
+        else:
+            # New group
+            n_group = Group(group_name)
+            n_group.update(g_data)
+            if n_group["size"] != 0:
+                existing_groups[group_name] = n_group
+
+        try:
+            return request.response(existing_groups[group_name])
+        except KeyError:
+            return request.response({"name":group_name, "size":0})
+
 
 
 # Returns a dict of new data gotten from the given args
 # Raises a status exception if args are incorrect
 def _getGroupDataFromArgs(args):
-  group = {}
-  n_args = len(args)
+    """
+    Parse and validate arguments to create a group dictionary.
 
-  # Get a size value
-  if "size" in args:
-    try:
-      group["size"] = int(args["size"][0])
-    except ValueError:
-      raise restlite.Status, "400 Size must be integer"
-    n_args = n_args - 1
+    The parameter ``args`` should be in the format returned by
+    :func:`urlparse.parse_qs`. It can have any of the following:
 
-  # Add workload list
-  if "workloads" in args:
-    group["workloads"] = args["workloads"]
-    n_args = n_args - 1
+    :param size: Group size.
+    :type size: ``int``
+    :param workloads: Attached workloads.
+    :type workloads: ``list(str)``
+    :param filters: Mapping restrictions.
+    :type filters: ``list(str)``
+    :returns: A dictionary with extracted group data.
+    :rtype: ``dict``
+    :raises: :exc:`restlite.Status` 400 if the size is invalid.
 
-  # Add filter list
-  if "filters" in args:
-    group["workloads"] = args["filters"]
-    n_args = n_args - 1
+             :exc:`restlite.Status` 400 if unrecognised arguments are included.
+    """
 
-  if n_args > 0:
-    raise restlite.Status, "400 Unrecognised Arguments"
+    group = {}
+    n_args = len(args)
 
-  return group
+    # Get a size value
+    if "size" in args:
+        try:
+            group["size"] = int(args["size"][0])
+        except ValueError:
+            raise restlite.Status, "400 Size must be integer"
+        n_args = n_args - 1
+
+    # Add workload list
+    if "workloads" in args:
+        group["workloads"] = args["workloads"]
+        n_args = n_args - 1
+
+    # Add filter list
+    if "filters" in args:
+        group["workloads"] = args["filters"]
+        n_args = n_args - 1
+
+    if n_args > 0:
+        raise restlite.Status, "400 Unrecognised Arguments"
+
+    return group
+
 
 def getGroupFromRank(rank):
-  # Don't call this during the setup phase; it doesn't make sense!
-  group_data = config.getSettings("groups",True)
-  
-  for group_name in group_data:
-    if group_data[group_name]['group_number'] == rank:
-      return group_data[group_name]
-      
-  raise restlite.Status, "500 Tried to find physical group number which doesn't exist"
+    """
+    Find a group from it's rank (given by vazels).
+
+    Don't call this during the setup phase; it doesn't make sense!
+
+    :param rank: The rank to find the group for.
+    :type rank: ``int``
+    :returns: The group with the given rank.
+    :rtype: :class:`Group`
+    :raises: :exc:`restlite.Status` 404 if the group rank doesn't exist.
+    """
+
+    # Don't call this during the setup phase; it doesn't make sense!
+    group_data = config.getSettings("groups",True)
+
+    for group_name in group_data:
+        if group_data[group_name]['group_number'] == rank:
+            return group_data[group_name]
+
+    raise restlite.Status, "404 Tried to find physical group number which doesn't exist"
