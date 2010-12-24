7dfe9face650cc063e1d253ffcd0d950d3d75c2e
Andy <as.gurden@gmail.com>
2010-12-24 00:39:24 +0000

    Added intersphinx to link modules to the python std library.

--- utilities/pydocs/source/conf.py
+++ utilities/pydocs/source/conf.py
@@ -25,7 +25,7 @@ needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -228,3 +228,7 @@ coverage_ignore_classes = []
 # -------- Options for TODO ----------
 
 todo_include_todos = True
+
+# --------- Options for intersphinx ----------
+
+intersphinx_mapping = {'python': ('http://docs.python.org', None)}
