eb9ca134359f7c9f4b0252aa828d5498e8641979
Andy <as.gurden@gmail.com>
2010-12-28 17:04:24 +0000

    Added documentation to the software grabber module

--- reef/softwaregrabber.py
+++ reef/softwaregrabber.py
@@ -1,6 +1,24 @@
+"""
+:synopsis: Allows software to be downloaded such as Vazels or the Java runtime.
+
+The idea originally was that this could manage the directories used for the
+software too so stubs could be placed here as placeholders. Unfortunately
+although there is one stub, I don't think it's used anywhere so this module is
+pretty useless.
+
+"""
+
 import config
 import os
 
 
 def getVazelsPath():
-  return os.path.join(config.getSettings("global")['basedir'],"vazels")
+    """
+    Get the path to the base directory of the Vazels installation.
+
+    :returns: Path to the Vazels installation directory.
+    :rtype: ``str``
+
+    """
+
+    return os.path.join(config.getSettings("global")['basedir'],"vazels")
