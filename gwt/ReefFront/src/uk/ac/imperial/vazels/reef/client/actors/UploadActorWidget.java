256010fd6b9a1daa1c028fa966c92fb433a816b9
Chris <chris@chris-VirtualBox.(none)>
2011-01-02 22:22:24 +0000

    Tidying, refactoring and bug removing of Upload widget classes

--- gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/actors/UploadActorWidget.java
+++ gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/actors/UploadActorWidget.java
@@ -5,10 +5,10 @@ import java.util.Set;
 import uk.ac.imperial.vazels.reef.client.AddressResolution;
 import uk.ac.imperial.vazels.reef.client.managers.MissingRequesterException;
 import uk.ac.imperial.vazels.reef.client.managers.PullCallback;
-import uk.ac.imperial.vazels.reef.client.workloads.WorkloadManager;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FileUpload;
@@ -32,7 +32,6 @@ public class UploadActorWidget extends Composite {
   void initPanel() {
     final FormPanel formPanel = new FormPanel();    
     initWidget(formPanel);
-
     formPanel.setAction(new AddressResolution().resolve("/actors"));
     
     VerticalPanel uploadPanel = new VerticalPanel();
@@ -46,65 +45,88 @@ public class UploadActorWidget extends Composite {
     actor_file.setName("actor_file");//for server
     uploadPanel.add(new Label("Actor file: "));
     uploadPanel.add(actor_file);
-    //validation here
 
-    TextBox actor_name = new TextBox();
+    //to give name to uploaded file
+    final TextBox actor_name = new TextBox();
     actor_name.setName("actor_name");//for server
     actor_name.setText("");
 //maybe create a listener that automatically puts filename as the name
     uploadPanel.add(new Label("Actor name: "));
     uploadPanel.add(actor_name);
 
+    //to get type of uploaded file, perhaps should have some validation based on the file type
     final ListBox actorLanguage = new ListBox();
     actorLanguage.addItem("JAVA");
     actorLanguage.addItem("PYTHON");
     actorLanguage.addItem("SUE");
     uploadPanel.add(new Label("Actor type: "));
     uploadPanel.add(actorLanguage);
-    
-    final TextBox actorLang = new TextBox(); //for server
+
+    //for server, create TextBox for data from ListBox
+    final TextBox actorLang = new TextBox();
     actorLang.setName("actor_type"); 
     uploadPanel.add(actorLang);
     actorLang.setVisible(false);
-
+    //set this TextBox to have ListBox data onSubmit
     formPanel.addSubmitHandler(new SubmitHandler() {
       public void onSubmit(SubmitEvent event) {
         actorLang.setText(actorLanguage.getValue(actorLanguage.getSelectedIndex()));
       }
     });
     
+    //tell manager that actor has been uploaded and add new item to onscreen list of actors
     formPanel.addSubmitCompleteHandler(new SubmitCompleteHandler() {
       public void onSubmitComplete(SubmitCompleteEvent event) {
         ActorManager.getManager().actorUploaded(event.getResults());
         try {
           ActorManager.getManager().getServerData();
-          actorList.addItem(actorLang.getText());
+          actorList.addItem(actor_name.getText());
+          actor_name.setText("");
         } catch (MissingRequesterException e) {
           e.printStackTrace();
         }
       }
     });
 
+    //actorList to hold a list of all uploaded actors
+    uploadPanel.add(new Label("Previously uploaded actors"));
+    actorList = new ListBox();
+    populateActorList();
+    uploadPanel.add(actorList);
+    
+    //button used when ready to submit all data
     Button button = new Button ("Submit", new ClickHandler() {
       public void onClick(ClickEvent event) {
-        formPanel.submit();
+        if(validateActorName(actor_name.getText())) {
+          formPanel.submit();
+        }
       }
     });
     uploadPanel.add(button);
+  }
 
-    actorList = new ListBox();
-    populateActorList();
-    uploadPanel.add(new Label("Previously uploaded actors"));
-    uploadPanel.add(actorList);
+  //validate name of actor, checking non-empty and unique
+  private boolean validateActorName(String actorName) {
+    if(ActorManager.getManager().getNames().contains(actorName)) {
+      Window.alert("You already have a group named '"+actorName+"'.");
+      return false;
+    }
+    else if(actorName.equals("")) {
+      Window.alert("Must give actor a name");
+      return false;
+    }
+    //groups were alphanumeric, is this something enforced for actors?
+    return true;
   }
   
+  //used for initial pulling of currently existing actors from the server
   private void populateActorList() {
     final ActorManager man = ActorManager.getManager();
     try {
       //get the list of actors from the server and add them to actorList
       man.withServerData(new PullCallback() {
         public void got() {
-          Set<String> actors = man.getNames(); //returns Set<String>
+          Set<String> actors = man.getNames();
           for(String actor : actors) {
             actorList.addItem(actor);
           }
--- gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/workloads/WorkloadWidget.java
+++ gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/workloads/WorkloadWidget.java
@@ -8,6 +8,7 @@ import uk.ac.imperial.vazels.reef.client.managers.PullCallback;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FileUpload;
@@ -19,8 +20,7 @@ import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteEvent;
 
-//error of not reaching server must be displayed
-//also when 2 wklds given same name
+//TODO: error of not reaching server must be displayed
 
 public class WorkloadWidget extends Composite {
   ListBox listWklds;
@@ -30,10 +30,12 @@ public class WorkloadWidget extends Composite {
   }
 
   void initPanel() {
+    //create FormPanel to send to server
     final FormPanel formPanel = new FormPanel();    
     initWidget(formPanel);
-
     formPanel.setAction(new AddressResolution().resolve("/workloads"));
+    
+    //on submission to server, inform manager that workload was uploaded
     formPanel.addSubmitCompleteHandler(new SubmitCompleteHandler() {
       public void onSubmitComplete(SubmitCompleteEvent event) {
         WorkloadManager.getManager().workloadUploaded(event.getResults());
@@ -45,6 +47,7 @@ public class WorkloadWidget extends Composite {
       }
     });
 
+    //principal display panel for widget
     VerticalPanel uploadPanel = new VerticalPanel();
     formPanel.setWidget(uploadPanel); 
 
@@ -52,47 +55,71 @@ public class WorkloadWidget extends Composite {
     formPanel.setEncoding(FormPanel.ENCODING_MULTIPART);
     formPanel.setMethod(FormPanel.METHOD_POST);
 
-    FileUpload wkld_file = new FileUpload();
+    //to select file to upload
+    final FileUpload wkld_file = new FileUpload();
     wkld_file.setName("wkld_file");
     uploadPanel.add(new Label("Workload file: "));
     uploadPanel.add(wkld_file);
-    //validation for .wkld needed
-
+    
+    //to name file to be uploaded
+    uploadPanel.add(new Label("Workload name: "));
     final TextBox wkld_name = new TextBox();
-
     wkld_name.setName("wkld_name");
     wkld_name.setText("");
-    //maybe create a listener that automatically puts filename as the name
-    uploadPanel.add(new Label("Workload name: "));
+    //maybe create a listener that automatically puts filename -.wkld as the name
     uploadPanel.add(wkld_name);
 
-    // eventually a textbox for workload writing here
+    //eventually a textbox for workload writing here
 
-    //listbox to display list of workloads    
+    //ListBox to display list of workloads    
     listWklds = new ListBox();
     uploadPanel.add(new Label("Previously uploaded workloads: "));
     uploadPanel.add(listWklds);
     populateListWklds();
 
+    //button to submit all the above when ready
+    //validation occurs when pressed prior to form submission
     Button button = new Button ("Submit", new ClickHandler() {
       public void onClick(ClickEvent event) {
-        formPanel.submit();
-        //TODO: HANDLER!!!
-        //assuming success, give new workload to workloads class
-        listWklds.addItem(wkld_name.getText());
-        wkld_name.setText("");
+        if(validateWorkloadName(wkld_name.getText())) {
+          if(wkld_file.getFilename().endsWith(".wkld")) {
+            formPanel.submit();
+          }
+          else {
+            Window.alert("Workloads must be of type: .wkld");
+          }
+          //assuming success, give new workload to workloads class
+          listWklds.addItem(wkld_name.getText());
+          wkld_name.setText("");
+        }
       }
+
     });
     uploadPanel.add(button);  
   }
 
+  //validate name of workload, checking non-empty and unique
+  private boolean validateWorkloadName(String wkldName) {
+    if(WorkloadManager.getManager().getNames().contains(wkldName)) {
+      Window.alert("You already have a group named '"+wkldName+"'.");
+      return false;
+    }
+    if(wkldName.equals("")) {
+      Window.alert("Must have non-empty workload name");
+      return false;
+    }
+    //groups were alphanumeric, is this something enforced for workloads?
+    return true;
+  }
+  
+  //initial pulling of list of workloads from server, to populate listWklds
   private void populateListWklds() {
     final WorkloadManager man = WorkloadManager.getManager();
     try {
       //get the list of workloads from the server and add them to wkldsBox
       man.withServerData(new PullCallback() {
         public void got() {
-          Set<String> workloads = man.getNames(); //returns Set<String>
+          Set<String> workloads = man.getNames();
           for(String wkld : workloads) {
             listWklds.addItem(wkld);
           }
