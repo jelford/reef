300a75dde3cb1e38336870558a1fdbcb20c7b7af
Andy <as.gurden@gmail.com>
2011-01-02 02:48:31 +0000

    Simplified the UI in control widget

--- gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/servercontrol/ServerControlWidget.java
+++ gwt/ReefFront/src/uk/ac/imperial/vazels/reef/client/servercontrol/ServerControlWidget.java
@@ -2,19 +2,23 @@ package uk.ac.imperial.vazels.reef.client.servercontrol;
 
 import uk.ac.imperial.vazels.reef.client.managers.IManager;
 import uk.ac.imperial.vazels.reef.client.managers.ManagerChangeHandler;
+import uk.ac.imperial.vazels.reef.client.managers.MissingRequesterException;
+import uk.ac.imperial.vazels.reef.client.managers.PullCallback;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiFactory;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ToggleButton;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
-public class ServerControlWidget extends Composite {
+public class ServerControlWidget extends Composite implements ManagerChangeHandler {
 
   private static ServerControlUiBinder uiBinder = GWT
   .create(ServerControlUiBinder.class);
@@ -22,26 +26,29 @@ public class ServerControlWidget extends Composite {
   interface ServerControlUiBinder extends UiBinder<Widget, ServerControlWidget> {
   }
 
-  private static final ServerControlStrings sStringConstants = 
-    (ServerControlStrings) GWT.create(ServerControlStrings.class);
-
-  @UiField Button btnToggleControlCenterRunning;
-  @UiField Label lblProbeInstructions;
+  @UiField ToggleButton btnToggleControlCenterRunning;
+  @UiField VerticalPanel runningControls;
   @UiField CheckBox ckbDoneWithProbes;
   @UiField Button btnStartExperiment;
-  
-  private boolean mServerRunning;
-  
-  private final ManagerChangeHandler mStatusChangeHandler;
 
   public ServerControlWidget() {
     initWidget(uiBinder.createAndBindUi(this));
     
-    mStatusChangeHandler = new StatusChangeHandler();
-    
     ServerStatusManager man = ServerStatusManager.getManager();
-    man.addChangeHandler(mStatusChangeHandler);
+    man.addChangeHandler(this);
     man.setAutoRefresh(true);
+    
+    try {
+      man.withServerData(new PullCallback() {
+        @Override
+        public void got() {
+          change(null);
+        }
+      });
+    } catch (MissingRequesterException e) {
+      e.printStackTrace();
+    }
+    
   }
 
   /*
@@ -50,10 +57,11 @@ public class ServerControlWidget extends Composite {
   @UiHandler("btnToggleControlCenterRunning")
   void toggleControlCenter(ClickEvent event) {
     btnToggleControlCenterRunning.setEnabled(false);
-    if (mServerRunning) {
-      ControlCentreManager.getManager().stop();
-    } else {
+    
+    if (btnToggleControlCenterRunning.getValue()) {
       ControlCentreManager.getManager().start();
+    } else {
+      ControlCentreManager.getManager().stop();
     }
   }
   
@@ -73,37 +81,53 @@ public class ServerControlWidget extends Composite {
    * @param running
    */
   private void setRunningState(boolean running) {
-    mServerRunning = running;
-    
     if (running) {
-      btnToggleControlCenterRunning.setText(sStringConstants.goBackToSetup());
-    } else {
-      btnToggleControlCenterRunning.setText(sStringConstants.startControlCentre());
+      ckbDoneWithProbes.setValue(false);
     }
     
     btnToggleControlCenterRunning.setEnabled(true);
-    
-    lblProbeInstructions.setVisible(running);
-    ckbDoneWithProbes.setVisible(running);
-    ckbDoneWithProbes.setValue(false);
-    btnStartExperiment.setVisible(running);
+    btnToggleControlCenterRunning.setDown(running);
+    runningControls.setVisible(running);
+  }
+  
+  /**
+   * Set the widget to be in intermediate state.
+   * When the state is not ready or running. Instead it is starting.
+   */
+  protected void setIntermediateState() {
+    btnToggleControlCenterRunning.setDown(true);
+    btnToggleControlCenterRunning.setEnabled(false);
+    runningControls.setVisible(false);
   }
   
   /**
    * Handle updates to server status
    */
-  private class StatusChangeHandler implements ManagerChangeHandler {
-    @Override
-    public void change(IManager man) {
-      ServerStatusManager statusManager = ServerStatusManager.getManager();
-      switch (statusManager.getStatus()) {
-      case RUNNING :
-        setRunningState(true);
-        break;
-      case READY :
-        setRunningState(false);
-        break;
-      }
+  @Override
+  public void change(IManager man) {
+    ServerStatusManager statusManager = ServerStatusManager.getManager();
+    
+    switch (statusManager.getStatus()) {
+    case RUNNING:
+      setRunningState(true);
+      break;
+    case READY:
+      setRunningState(false);
+      break;
+    case STARTING:
+      setIntermediateState();
+      break;
     }
+  }
+  
+  /**
+   * Create the toggle button with separate up and down text.
+   * @param up Up text.
+   * @param down Down text.
+   * @return The toggle button.
+   */
+  @UiFactory
+  ToggleButton createToggle(String up, String down) {
+    return new ToggleButton(up, down);
   }
 }
